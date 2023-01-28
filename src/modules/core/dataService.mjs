b7dec4e3f6224bcee5cf32c0eb5459e0c47c1cf6
Lee Howard <git@leehoward.dev>
2023-01-28 13:44:26 -0800

    add try/catch to getData.mjs
    - exception handling required to pass tests

--- src/modules/core/dataService.mjs
+++ src/modules/core/dataService.mjs
@@ -1,10 +1,15 @@
 // dataService.mjs
 
 const getData = async (provider, url) => {
-    const res = await provider(url);
-    const data = await res.json();
-    return data;
+    try {
+        const res = await provider(url);
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
 }
+
 export const DataService = {
     getData: getData,
 }
