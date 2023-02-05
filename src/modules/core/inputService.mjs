3510b1cd2741d74c6eb578bba24cc941bef9fee6
leehowardx <git@leehoward.dev>
2023-02-04 17:55:55 -0800

    add working validation in input pattern
    - remove validate js code since wasnt working correctly

--- src/modules/core/inputService.mjs
+++ src/modules/core/inputService.mjs
@@ -10,24 +10,12 @@ const getInput = (callback) => {
   // TODO: Refractor to array of addEventListener?
     formSubmit.addEventListener("submit", (event) => {
         console.log('username submitted');
+        // console.log(validateInput(nameInput.value));
         nameInput.blur();
         event.preventDefault();
         callback(nameInput.value);
   });
 };
-
-// const validateInput = (input) => {
-//   // valid github usernames return true else return false
-//   return /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])[\s]){0,38}$/i.test(input);
-// };
-
-// const handleInput = (callback) => {
-//   let input = nameInput.value;
-// //   let isValid = false;
-// //   if (validateInput(input)) isValid = true;
-
-//   callback(input);
-// };
 
 export const InputService = {
   getInput: getInput,
