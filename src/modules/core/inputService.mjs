64e766b6945708a5eb4cc6e70ffc67388f890533
leehowardx <git@leehoward.dev>
2023-02-05 03:06:00 -0800

    modify input validation yet again
    - move all validation to js

--- src/modules/core/inputService.mjs
+++ src/modules/core/inputService.mjs
@@ -4,10 +4,40 @@ const nameInput = document.querySelector("#username-input");
 const formSubmit = document.querySelector("#submit-form");
 
 const getInput = (callback) => {
+    // set regexpr pattern input validation
+    nameInput.setAttribute(
+        "pattern",
+        // TODO: explain what this does step by step
+        "^[A-Za-z\\d](?:[A-Za-z\\d]|-(?=[A-Za-z\\d])){0,38}$"
+    );
+    
+    // once its invalid 
+    nameInput.addEventListener("invalid", () => {
+        const validityState = nameInput.validity;
+
+        if (validityState.valueMissing) {
+            nameInput.setCustomValidity("required");
+        } else if (validityState.patternMismatch) {
+            nameInput.setCustomValidity("enter valid github username");
+        } else if (validityState.tooShort) {
+            nameInput.setCustomValidity("too short");
+        } else if (validityState.tooLong) {
+            nameInput.setCustomValidity("too long");
+        }
+    });
+
+    // remove validation tooltip when typing again unless still error
+    nameInput.addEventListener("input", () => {
+        nameInput.setCustomValidity("");
+    });
+
+    // submit form after validation pass. send callback
     formSubmit.addEventListener("submit", (event) => {
-        console.log("username submitted");
+        console.info("username submitted");
+
         nameInput.blur();
         event.preventDefault();
+        formSubmit.checkValidity();
         callback(nameInput.value);
     });
 };
