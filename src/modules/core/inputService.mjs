5d2828a31d71bf1186cd94240173ca92bd4f31ce
leehowardx <git@leehoward.dev>
2023-02-05 00:34:23 -0800

    cleanup inputService.mjs
    - remove element definitions

--- src/modules/core/inputService.mjs
+++ src/modules/core/inputService.mjs
@@ -2,12 +2,8 @@
 
 const nameInput = document.querySelector("#username-input");
 const formSubmit = document.querySelector("#submit-form");
-const searchBtn = document.querySelector("#searchBtn");
-const colorMode = document.querySelector("#color-mode");
-const html = document.querySelector("html");
 
 const getInput = (callback) => {
-  // TODO: Refractor to array of addEventListener?
     formSubmit.addEventListener("submit", (event) => {
         console.log('username submitted');
         nameInput.blur();
