374e72bd1b956b0f04ba2da01c87d772e349050e
Lee Howard <git@leehoward.dev>
2023-02-11 11:07:17 -0800

    fail test to check if build fails

--- test/unit/utilityService.test.mjs
+++ test/unit/utilityService.test.mjs
@@ -3,9 +3,9 @@
 import { UtilityService } from "../../src/modules/core/utilityService.mjs";
 
 test("constructUrl() takes string and returns valid url", () => {
-    let username = `leehowardx`;
-    let url = `https://api.github.com/search/commits\
-?q=author:leehowardx&order=asc\
+    let username = `lee0x1`;
+    let url = `https:/api.github.com/search/commits\
+?q=author:lee0x1&order=asc\
 &sort=committer-date`;
 
     expect(UtilityService.constructUrl(username)).toBe(url);
