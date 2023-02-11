5aac580aea141aa875d2bff6bebb20271922c3d6
Lee Howard <git@leehoward.dev>
2023-02-11 11:09:31 -0800

    pass all tests again. check done

--- test/unit/utilityService.test.mjs
+++ test/unit/utilityService.test.mjs
@@ -4,7 +4,7 @@ import { UtilityService } from "../../src/modules/core/utilityService.mjs";
 
 test("constructUrl() takes string and returns valid url", () => {
     let username = `lee0x1`;
-    let url = `https:/api.github.com/search/commits\
+    let url = `https://api.github.com/search/commits\
 ?q=author:lee0x1&order=asc\
 &sort=committer-date`;
 
