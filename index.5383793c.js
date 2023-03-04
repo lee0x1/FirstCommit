48f998b0982454f6d0379b17e0ad6aaff8bccfe6
lee0x1 <leemasterhoward@outlook.com>
2023-03-04 05:43:25 +0000

    Deploying to gh-pages from @ lee0x1/fc@40499d8c298bf303fde720a3724c046b33ee69c9 🚀

--- index.5383793c.js
+++ index.5383793c.js
@@ -6,4 +6,4 @@
  * Modified by Lee Howard - 2/3/23
  */
 (()=>{const e=localStorage.getItem("theme"),t=document.querySelector("#color-mode-label"),o=()=>e||(window.matchMedia("(prefers-color-scheme: light)").matches?"light":"dark"),d=function(e){"light"===e&&window.matchMedia("(prefers-color-scheme: light)").matches?(document.documentElement.setAttribute("data-bs-theme","light"),t.textContent="Light Mode"):(document.documentElement.setAttribute("data-bs-theme","dark"),t.textContent=" Dark Mode")};window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change",(()=>{"light"===e&&"dark"===e||d(o())})),window.addEventListener("DOMContentLoaded",(()=>{d(o());const e=document.querySelector("#color-mode");"dark"===o()&&(e.checked=!0),e.addEventListener("click",(e=>{e.target.checked?document.documentElement.setAttribute("data-bs-theme","dark"):document.documentElement.setAttribute("data-bs-theme","light");const t=document.documentElement.getAttribute("data-bs-theme");localStorage.setItem("theme",t),d(t),console.log(t)}))}))})();
-//# sourceMappingURL=index.a5c6929a.js.map
+//# sourceMappingURL=index.5383793c.js.map
