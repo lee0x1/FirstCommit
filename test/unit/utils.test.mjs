0150ee2a2a329d68678ad2360f22f2e68de0d5f9
Lee Howard <leegibsonhoward@gmail.com>
2024-05-04 12:59:31 -0700

    update username
    - change from lee0x1 to leegibsonhoward

--- test/unit/utils.test.mjs
+++ test/unit/utils.test.mjs
@@ -3,9 +3,9 @@
 import { Utils } from "../../src/modules/core/utils.mjs";
 
 test("constructUrl() takes string and returns valid url", () => {
-    let username = `lee0x1`;
+    let username = `leegibsonhoward`;
     let url = `https://api.github.com/search/commits\
-?q=author:lee0x1&order=asc\
+?q=author:leegibsonhoward&order=asc\
 &sort=committer-date`;
 
     expect(Utils.constructUrl(username)).toBe(url);
